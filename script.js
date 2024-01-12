a0c36173e9b1b40a3016aae32698c8b00880bd72
Sean <130804310+Seanhru@users.noreply.github.com>
2024-01-11 22:05:47 -0500

    added data for stock prices

---
+++ script.js
@@ -0,0 +1,84 @@
+// Apple
+const appleStock = {
+    name: 'Apple',
+    2020: 75,
+    2023: 130
+  };
+  
+  // Google
+  const googleStock = {
+    name: 'Google',
+    2020: 68,
+    2023: 88
+  };
+  
+  // Amazon
+  const amazonStock = {
+    name: 'Amazon',
+    2020: 94,
+    2023: 84
+  };
+  
+  // Capital One
+  const capitalOneStock = {
+    name: 'Capital One',
+    2020: 102,
+    2023: 50000
+  };
+  
+  // JP Morgan
+  const jpMorganStock = {
+    name: 'JP Morgan',
+    2020: 138,
+    2023: 137
+  };
+  
+  // Niko
+  const nikoStock = {
+    name: 'Niko',
+    2020: 50,
+    2023: 85
+  };
+  
+  // Walmart
+  const walmartStock = {
+    name: 'Walmart',
+    2020: 119,
+    2023: 141
+  };
+  
+  // Target
+  const targetStock = {
+    name: 'Target',
+    2020: 129,
+    2023: 149
+  };
+  
+  // Costco
+  const costcoStock = {
+    name: 'Costco',
+    2020: 290,
+    2023: 456
+  };
+  
+  // Dominoes
+  const dominosStock = {
+    name: 'Dominoes',
+    2020: 290,
+    2023: 346
+  };
+  
+  // McDonald's
+  const mcdonaldsStock = {
+    name: "McDonald's",
+    2020: 198,
+    2023: 263
+  };
+  
+  // Chipotle
+  const chipotleStock = {
+    name: 'Chipotle',
+    2020: 850,
+    2023: 1400
+  };
+  
