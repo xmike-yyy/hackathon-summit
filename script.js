9bacd8ea2ebf94872accf9b14867c7e21e2f5669
devon wallerson <136762013+devonwallerson@users.noreply.github.com>
2024-01-11 23:12:28 -0500

    script.js

--- script.js
+++ script.js
@@ -82,39 +82,99 @@ const appleStock = {
     2023: 1400
   };
 
+ var budget1 = document.getElementById('player1Budget');
+  var budget2 = document.getElementById('player2Budget');
+  var verified = false;
+
+
 document.getElementById('calculateButton').addEventListener('click', function() {
     // Simplified JavaScript to fetch values from stock inputs
 
-    var p1Apple = document.getElementById('player1AppleStocks').value;
-    var p2Apple = document.getElementById('player2AppleStocks').value;
-    var p1Google = document.getElementById('player1GoogleStocks').value;
-    var p2Google = document.getElementById('player2GoogleStocks').value;
-    var p1Amazon = document.getElementById('player1AmazonStocks').value;
-    var p2Amazon = document.getElementById('player2AmazonStocks').value;
+    budget1.textContent = '$10,000'; // Or use innerHTML if you need to include HTML tags
+    budget1.style.color = 'white';
+  
+    budget2.textContent = '$10,000'; // Or use innerHTML if you need to include HTML tags
+    budget2.style.color = 'white';
 
-    var p1CapitalOne = document.getElementById('player1CapitalOneStocks').value;
-    var p2CapitalOne = document.getElementById('player2CapitalOneStocks').value;
-    var p1JPMorgan = document.getElementById('player1jpMorganStocks').value;
-    var p2JPMorgan = document.getElementById('player2jpMorganStocks').value;
-    var p1MS = document.getElementById('player1MSStocks').value;
-    var p2MS = document.getElementById('player2MSStocks').value;
+    let p1 = new Array(12);
+    let p2 = new Array(12);
 
-    var p1Walmart = document.getElementById('player1WalmartStocks').value;
-    var p2Walmart = document.getElementById('player2WalmartStocks').value;
-    var p1Costco = document.getElementById('player1CostcoStocks').value;
-    var p2Costco = document.getElementById('player2CostcoStocks').value;
-    var p1Target = document.getElementById('player1TargetStocks').value;
-    var p2Target = document.getElementById('player2TargetStocks').value;
+    p1[0] = document.getElementById('player1AppleStocks').value * appleStock['2020'];
+    p2[0] = document.getElementById('player2AppleStocks').value * appleStock['2020'];
 
-    var p1FiveGuys = document.getElementById('player1FiveGuysStocks').value;
-    var p2FiveGuys = document.getElementById('player2FiveGuysStocks').value;
-    var p1McD = document.getElementById('player1mcDStocks').value;
-    var p2McD = document.getElementById('player2mcDStocks').value;
-    var p1Chipotle = document.getElementById('player1ChipotleStocks').value;
-    var p2Chipotle = document.getElementById('player2ChipotleStocks').value;
+    p1[1] = document.getElementById('player1GoogleStocks').value * googleStock['2020'];
+    p2[1] = document.getElementById('player2GoogleStocks').value * googleStock['2020'];
+
+    p1[2] = document.getElementById('player1AmazonStocks').value * amazonStock['2020'];
+    p2[2] = document.getElementById('player2AmazonStocks').value * amazonStock['2020'];
+
+    p1[3] = document.getElementById('player1CapitalOneStocks').value * capitalOneStock['2020'];
+    p2[3] = document.getElementById('player2CapitalOneStocks').value * capitalOneStock['2020'];
+
+    p1[4] = document.getElementById('player1jpMorganStocks').value * jpMorganStock['2020'];
+    p2[4] = document.getElementById('player2jpMorganStocks').value * jpMorganStock['2020'];
+
+    p1[5] = document.getElementById('player1MSStocks').value * nikoStock['2020'];
+    p2[5] = document.getElementById('player2MSStocks').value * nikoStock['2020'];
+
+    p1[6] = document.getElementById('player1WalmartStocks').value * walmartStock['2020'];
+    p2[6] = document.getElementById('player2WalmartStocks').value * walmartStock['2020'];
+
+    p1[7] = document.getElementById('player1CostcoStocks').value * targetStock['2020'];
+    p2[7] = document.getElementById('player2CostcoStocks').value * targetStock['2020'];
+
+    p1[8] = document.getElementById('player1TargetStocks').value * costcoStock['2020'];
+    p2[8] = document.getElementById('player2TargetStocks').value * costcoStock['2020'];
+
+    p1[9] = document.getElementById('player1FiveGuysStocks').value * dominosStock['2020'];
+    p2[9] = document.getElementById('player2FiveGuysStocks').value * dominosStock['2020'];
+
+    p1[10] = document.getElementById('player1mcDStocks').value * mcdonaldsStock['2020'];
+    p2[10] = document.getElementById('player2mcDStocks').value * mcdonaldsStock['2020'];
+
+    p1[11] = document.getElementById('player1ChipotleStocks').value * chipotleStock['2020'];
+    p2[11] = document.getElementById('player2ChipotleStocks').value * chipotleStock['2020'];
 
     var player1Budget = 10000;
-    
+    var player2Budget = 10000;
+
+    var totalp1 = 0, totalp2 = 0;
+
+    p1.forEach(function(input) {
+        totalp1 += input;
+    });
+
+    p2.forEach(function(input) {
+        totalp2 += input;
+    });
+
+
+
+    if(totalp1 > player1Budget && totalp2 > player2Budget){
+        budget1.innerHTML = 'Overbudget';
+        budget2.innerHTML = 'Overbudget';
+        budget1.style.color = 'red';
+        budget2.style.color = 'red';
+        verified = false;
+    } else if (totalp1 > player1Budget){
+        budget1.innerHTML = 'Overbudget';
+        budget1.style.color = 'red';
+
+        budget2.innerHTML = 10000 - totalp2;
+        verified = false;
+    } else if (totalp2 > player2Budget) {
+        budget2.style.color = 'red';
+        budget2.innerHTML = 'Overbudget';
+
+        budget1.innerHTML = 10000 - totalp1;
+        verified = false;
+    } else {
+        verified = true;
+
+        budget1.innerHTML = 10000 - totalp1;
+        budget2.innerHTML = 10000 - totalp2;
+    }
+       
 });
   
 function navigateToPage2() {
